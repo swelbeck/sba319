476809de052ce6b7568eb7367a79a87478ccb2bc
SWelbeck <swelbeck12@gmail.com>
2024-10-26 11:03:28 -0400

    updated data

--- data/countries.mjs
+++ data/countries.mjs
@@ -2,13 +2,303 @@
 
 export const countries = [
   {
-    name: "Country",
-    continent: "ContinentX",
-    capitalCity: "CapitalX",
+    name: "Afghanistan",
+    continent: "Asia",
+    capitalCity: "Kabul",
     languages: {
-      officialLanguage: "LanguageY",
+      officialLanguage: "Pashto",
+      otherLanguages: ["Dari"],
+    },
+    population: 38928346,
+  },
+  {
+    name: "Brazil",
+    continent: "South America",
+    capitalCity: "Brasília",
+    languages: {
+      officialLanguage: "Portuguese",
       otherLanguages: [],
     },
-    population: 500000,
+    population: 212559417,
+  },
+  {
+    name: "Canada",
+    continent: "North America",
+    capitalCity: "Ottawa",
+    languages: {
+      officialLanguage: "English",
+      otherLanguages: ["French"],
+    },
+    population: 37742154,
+  },
+  {
+    name: "Denmark",
+    continent: "Europe",
+    capitalCity: "Copenhagen",
+    languages: {
+      officialLanguage: "Danish",
+      otherLanguages: [],
+    },
+    population: 5818553,
+  },
+  {
+    name: "Egypt",
+    continent: "Africa",
+    capitalCity: "Cairo",
+    languages: {
+      officialLanguage: "Arabic",
+      otherLanguages: [],
+    },
+    population: 102334404,
+  },
+  {
+    name: "Finland",
+    continent: "Europe",
+    capitalCity: "Helsinki",
+    languages: {
+      officialLanguage: "Finnish",
+      otherLanguages: ["Swedish"],
+    },
+    population: 5540720,
+  },
+  {
+    name: "Greece",
+    continent: "Europe",
+    capitalCity: "Athens",
+    languages: {
+      officialLanguage: "Greek",
+      otherLanguages: [],
+    },
+    population: 10423054,
+  },
+  {
+    name: "India",
+    continent: "Asia",
+    capitalCity: "New Delhi",
+    languages: {
+      officialLanguage: "Hindi",
+      otherLanguages: ["English"],
+    },
+    population: 1380004385,
+  },
+  {
+    name: "Japan",
+    continent: "Asia",
+    capitalCity: "Tokyo",
+    languages: {
+      officialLanguage: "Japanese",
+      otherLanguages: [],
+    },
+    population: 126476461,
+  },
+  {
+    name: "Kenya",
+    continent: "Africa",
+    capitalCity: "Nairobi",
+    languages: {
+      officialLanguage: "Swahili",
+      otherLanguages: ["English"],
+    },
+    population: 53771296,
+  },
+  {
+    name: "Laos",
+    continent: "Asia",
+    capitalCity: "Vientiane",
+    languages: {
+      officialLanguage: "Lao",
+      otherLanguages: [],
+    },
+    population: 7275560,
+  },
+  {
+    name: "Mexico",
+    continent: "North America",
+    capitalCity: "Mexico City",
+    languages: {
+      officialLanguage: "Spanish",
+      otherLanguages: [],
+    },
+    population: 128932753,
+  },
+  {
+    name: "Nigeria",
+    continent: "Africa",
+    capitalCity: "Abuja",
+    languages: {
+      officialLanguage: "English",
+      otherLanguages: ["Hausa", "Igbo", "Yoruba"],
+    },
+    population: 206139589,
+  },
+  {
+    name: "Oman",
+    continent: "Asia",
+    capitalCity: "Muscat",
+    languages: {
+      officialLanguage: "Arabic",
+      otherLanguages: [],
+    },
+    population: 5106626,
+  },
+  {
+    name: "Peru",
+    continent: "South America",
+    capitalCity: "Lima",
+    languages: {
+      officialLanguage: "Spanish",
+      otherLanguages: ["Quechua", "Aymara"],
+    },
+    population: 32971854,
+  },
+  {
+    name: "Qatar",
+    continent: "Asia",
+    capitalCity: "Doha",
+    languages: {
+      officialLanguage: "Arabic",
+      otherLanguages: ["English"],
+    },
+    population: 2881060,
+  },
+  {
+    name: "Russia",
+    continent: "Europe",
+    capitalCity: "Moscow",
+    languages: {
+      officialLanguage: "Russian",
+      otherLanguages: [],
+    },
+    population: 145912025,
+  },
+  {
+    name: "Spain",
+    continent: "Europe",
+    capitalCity: "Madrid",
+    languages: {
+      officialLanguage: "Spanish",
+      otherLanguages: ["Catalan", "Galician", "Basque"],
+    },
+    population: 46754778,
+  },
+  {
+    name: "Turkey",
+    continent: "Asia",
+    capitalCity: "Ankara",
+    languages: {
+      officialLanguage: "Turkish",
+      otherLanguages: [],
+    },
+    population: 84339067,
+  },
+  {
+    name: "Uganda",
+    continent: "Africa",
+    capitalCity: "Kampala",
+    languages: {
+      officialLanguage: "English",
+      otherLanguages: ["Swahili"],
+    },
+    population: 45741007,
+  },
+  {
+    name: "Vietnam",
+    continent: "Asia",
+    capitalCity: "Hanoi",
+    languages: {
+      officialLanguage: "Vietnamese",
+      otherLanguages: [],
+    },
+    population: 97338579,
+  },
+  {
+    name: "Yemen",
+    continent: "Asia",
+    capitalCity: "Sanaa",
+    languages: {
+      officialLanguage: "Arabic",
+      otherLanguages: [],
+    },
+    population: 29825964,
+  },
+  {
+    name: "Zimbabwe",
+    continent: "Africa",
+    capitalCity: "Harare",
+    languages: {
+      officialLanguage: "English",
+      otherLanguages: ["Shona", "Ndebele"],
+    },
+    population: 14862924,
+  },
+  {
+    name: "Australia",
+    continent: "Oceania",
+    capitalCity: "Canberra",
+    languages: {
+      officialLanguage: "English",
+      otherLanguages: [],
+    },
+    population: 25499884,
+  },
+  {
+    name: "Belgium",
+    continent: "Europe",
+    capitalCity: "Brussels",
+    languages: {
+      officialLanguage: "Dutch",
+      otherLanguages: ["French", "German"],
+    },
+    population: 11589623,
+  },
+  {
+    name: "Chile",
+    continent: "South America",
+    capitalCity: "Santiago",
+    languages: {
+      officialLanguage: "Spanish",
+      otherLanguages: [],
+    },
+    population: 19116201,
+  },
+  {
+    name: "France",
+    continent: "Europe",
+    capitalCity: "Paris",
+    languages: {
+      officialLanguage: "French",
+      otherLanguages: [],
+    },
+    population: 65273511,
+  },
+  {
+    name: "Germany",
+    continent: "Europe",
+    capitalCity: "Berlin",
+    languages: {
+      officialLanguage: "German",
+      otherLanguages: [],
+    },
+    population: 83783942,
+  },
+  {
+    name: "Iceland",
+    continent: "Europe",
+    capitalCity: "Reykjavik",
+    languages: {
+      officialLanguage: "Icelandic",
+      otherLanguages: [],
+    },
+    population: 366425,
+  },
+  {
+    name: "South Korea",
+    continent: "Asia",
+    capitalCity: "Seoul",
+    languages: {
+      officialLanguage: "Korean",
+      otherLanguages: [],
+    },
+    population: 51269185,
   },
 ];
